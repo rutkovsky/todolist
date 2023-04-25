da8598b4bf87c62b839e026958e99a47e1bda625
rutkovsky <rus.rutkovsky@yandex.ru>
2023-04-25 20:47:02 +0300

    PS-1

---
+++ src/Tasks.tsx
@@ -0,0 +1,34 @@
+type TasksType = {
+    taskId: number,
+    title: string,
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string,
+    tasks: TasksType[],
+    students: Array<string>
+}
+
+export const Tasks = (props: PropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <ul>
+                {props.tasks.map((el) => {
+                    return (
+                        <li key={el.taskId}><input type="checkbox" checked={el.isDone}/><span>{el.title}</span></li>)
+                })}
+            </ul>
+            <h3>Students</h3>
+            <ul>
+                {props.students.map((el) => {
+                    return (
+                        <li>{el}</li>
+                    )
+                }
+                    )}
+                    </ul>
+                    </div>
+                    )
+                }
